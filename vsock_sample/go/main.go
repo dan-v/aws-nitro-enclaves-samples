2ee018e918a5ea1edc72df3c51b39fb508389eab
Dan Vittegleo <dan@vittegleo.com>
2021-03-16 10:42:43 -0700

    example with a proxy to internal service in enclave

--- vsock_sample/go/main.go
+++ vsock_sample/go/main.go
@@ -3,28 +3,40 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/mdlayher/vsock"
 	"github.com/spf13/cobra"
+	"github.com/mdlayher/vsock"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
+	"sync"
 	"time"
 )
 
+const (
+	defaultPort = 5005
+	defaultURL = "http://127.0.0.1:8080"
+	defaultProxyAddress = "127.0.0.1:8080"
+)
+
 var (
-	cid, clientPort, serverPort uint32
+	cid, clientPort, serverListenPort uint32
+	clientURL, serverProxyAddress string
+	clientDebug, serverDebug bool
 )
 
 func init() {
 	rootCmd.AddCommand(serverCmd)
 	rootCmd.AddCommand(clientCmd)
 	clientCmd.Flags().Uint32VarP(&cid, "cid", "c", 0, "CID")
-	clientCmd.Flags().Uint32VarP(&clientPort, "port", "p", 0, "Remote Port")
+	clientCmd.Flags().Uint32VarP(&clientPort, "vsock-port", "v", defaultPort, "Remote Port")
+	clientCmd.Flags().StringVarP(&clientURL, "url", "u", defaultURL, "URL to request inside enclave")
+	clientCmd.Flags().BoolVarP(&clientDebug, "debug", "d", false, "Debug mode uses TCP rather than vsock")
 	clientCmd.MarkFlagRequired("cid")
-	clientCmd.MarkFlagRequired("port")
-	serverCmd.Flags().Uint32VarP(&serverPort, "port", "p", 0, "Listen Port")
-	serverCmd.MarkFlagRequired("port")
+	serverCmd.Flags().Uint32VarP(&serverListenPort, "listen-port", "l", defaultPort, "Listen Port")
+	serverCmd.Flags().StringVarP(&serverProxyAddress, "proxy-address", "p", defaultProxyAddress, "Address inside enclave to proxy connection to")
+	serverCmd.Flags().BoolVarP(&serverDebug, "debug", "d", false, "Debug mode uses TCP rather than vsock")
 }
 
 var rootCmd = &cobra.Command{
@@ -36,34 +48,88 @@ var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "run http server with vsock listener",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Printf("starting server on port %v\n", serverPort)
-		l, err := vsock.Listen(serverPort)
-		if err != nil {
-			log.Fatal(err)
+		go func() {
+			log.Printf("starting example server on %v", defaultProxyAddress)
+			tcpListener, err := net.Listen("tcp", defaultProxyAddress)
+			if err != nil {
+				log.Fatal(err)
+			}
+			mux := http.NewServeMux()
+			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "testing tcp server behind vsocks proxy\n")
+			})
+			log.Fatal(http.Serve(tcpListener, mux))
+		}()
+
+		var l net.Listener
+		var err error
+		if !serverDebug {
+			log.Printf("starting vsock proxy on port %v\n", serverListenPort)
+			l, err = vsock.Listen(serverListenPort)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			log.Printf("starting debug tcp proxy on port %v\n", serverListenPort)
+			l, err = net.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", serverListenPort))
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 		defer l.Close()
 
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "testing http with vsock\n")
-		})
-		log.Fatal(http.Serve(l, mux))
+		for {
+			log.Println("waiting for new client connection")
+			c, err := l.Accept()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			log.Println("accepted connection")
+			go handleConnection(c)
+		}
 	},
+}
+
+func handleConnection(client net.Conn) error {
+	defer client.Close()
+
+	target, err := net.Dial("tcp", serverProxyAddress)
+	if err != nil {
+		return fmt.Errorf("failed to connect to target: %w", err)
+	}
+	defer target.Close()
+	log.Printf("connected to proxy %v\n", serverProxyAddress)
+
+	log.Printf("proxying traffic from client to %v\n", serverProxyAddress)
+	close := sync.Once{}
+	go copy(client, target, close)
+	err = copy(target, client, close)
+	if err != nil {
+		return err
+	}
+	log.Printf("proxying traffic completed")
+
+	return nil
 }
 
 var clientCmd = &cobra.Command{
 	Use:   "client",
 	Short: "run http client over vsock connection",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Printf("client connecting to cid=%v port=%v\n", cid, clientPort)
 		tr := &http.Transport{
 			IdleConnTimeout:    15 * time.Second,
 			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				if clientDebug {
+					log.Printf("client connecting to debug tcp port=%v\n", clientPort)
+					return net.Dial("tcp", fmt.Sprintf("127.0.0.1:%v", clientPort))
+				}
+				log.Printf("client connecting to vsock cid=%v port=%v\n", cid, clientPort)
 				return vsock.Dial(cid, clientPort)
 			},
 		}
 		client := &http.Client{Transport: tr}
-		resp, err := client.Get("http://test/")
+		resp, err := client.Get(clientURL)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -83,3 +149,15 @@ func main() {
 	}
 }
 
+
+func copy(dst io.WriteCloser, src io.ReadCloser, close sync.Once) error {
+	_, err := io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+	close.Do(func() {
+		dst.Close()
+		src.Close()
+	})
+	return nil
+}
---
+++ vsock_sample/go/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ifconfig lo 127.0.0.1
+
+/app/vsocksample server
